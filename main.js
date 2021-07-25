d9fb27b02cd80aaccc728eaa84970729c981ab87
liu yi <yolaucn@gmail>
2021-07-25 11:35:41 +0800

    init

---
+++ main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+  data:function(){
+    return {
+      cart:[],
+      premium:false
+    }
+  },
+  methods:{
+    updateCart(id){
+      this.cart.push(id)
+    }
+  }
+});
